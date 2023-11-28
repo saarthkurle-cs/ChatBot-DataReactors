084f29f587c923aaf5afa9bad3e7104fd5e2eecf
student <gonzalezjoshua925@gmail.com>
2023-11-27 18:54:21 -0800

    home update with home js and css pages

---
+++ public/home.js
@@ -0,0 +1,30 @@
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () =>{
+    if(scrollY >= 180){
+        navbar.classList.add('bg')
+    }else{
+        navbar.classList.remove('bg')
+    }
+    console.log(scrollY);
+})
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
