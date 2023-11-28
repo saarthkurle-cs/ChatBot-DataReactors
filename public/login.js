0da95dad209cbc46f9a84504c09eb376ed11bb47
Zia-wang <xwang35@sfsu.edu>
2023-11-27 22:48:23 -0800

    changed style for login and account

--- public/login.js
+++ public/login.js
@@ -1,3 +1,4 @@
+// For sign in validation
 function validateLogin() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
@@ -9,25 +10,28 @@ function validateLogin() {
     window.location.href = "index.html";
 }
 
+// Send a password reset email
 function sendPasswordResetEmail() {
     // Forgot password 
 }
 
+// Create a new account
 function submitRegistration() {
     // Create account 
 }
 
+// Show the selected section
 function showSection(sectionId) {
     // Hide all sections
     document.getElementById('loginForm').style.display = 'none';
     document.getElementById('forgotPasswordSection').style.display = 'none';
     document.getElementById('createAccountSection').style.display = 'none';
-
+  
     // Show the selected section
     document.getElementById(sectionId).style.display = 'block';
-
+  
     // Reset messages
     document.getElementById('loginMessage').innerHTML = '';
     document.getElementById('forgotPasswordMessage').innerHTML = '';
     document.getElementById('createAccountMessage').innerHTML = '';
-}
+  }
