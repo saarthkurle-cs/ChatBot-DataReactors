030c2c6e8614bbd2bfc46c516d2dc0f4c966012b
Saarth Kurle <113139542+saarthkurle-cs@users.noreply.github.com>
2023-12-12 23:00:39 -0800

    Update login.js

--- public/login.js
+++ public/login.js
@@ -1,13 +1,31 @@
-// For sign in validation
 function validateLogin() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     var loginMessage = document.getElementById('loginMessage');
 
-    // Authentication
-    
-    // Go back to home page
-    window.location.href = "index.html";
+    // Make a POST request to the server
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: username, password: password }),
+    })
+    .then(response => {
+        if (response.ok) {
+            // Redirect to index.html on successful login
+            window.location.href = '/index.html';
+        } else {
+            // Display error message on invalid credentials
+            return response.text();
+        }
+    })
+    .then(message => {
+        loginMessage.innerHTML = message;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
 
 // Send a password reset email
